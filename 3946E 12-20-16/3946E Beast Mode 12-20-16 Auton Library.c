bc228859a9aa594ff92b7235b7b85300eaec7dd5
lheadlee19 <lheadlee19@kentdenver.org>
2017-01-03 18:05:43 -0700

    Quick Fix

---
+++ 3946E 12-20-16/3946E Beast Mode 12-20-16 Auton Library.c
@@ -0,0 +1,26 @@
+void redPoleMain(){
+}
+
+void redPoleAlt(){
+}
+
+void redLoadMain(){
+}
+
+void redLoadAlt(){
+}
+
+void bluePoleMain(){
+}
+
+void bluePoleAlt(){
+}
+
+void blueLoadMain(){
+}
+
+void blueLoadAlt(){
+}
+
+void programmingSkills(){
+}
